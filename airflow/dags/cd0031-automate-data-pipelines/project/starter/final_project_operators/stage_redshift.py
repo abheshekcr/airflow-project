c702c3131bedada272c3bd098088aa42bd7c3500
abheshekcr <cabheshek@gmail.com>
2024-04-04 19:02:46 +0530

    Update stage_redshift.py

--- airflow/dags/cd0031-automate-data-pipelines/project/starter/final_project_operators/stage_redshift.py
+++ airflow/dags/cd0031-automate-data-pipelines/project/starter/final_project_operators/stage_redshift.py
@@ -1,7 +1,8 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-from airflow.secrets.metastore import MetastoreBackend
+from udacity.common import final_project_sql_statements
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
@@ -11,9 +12,9 @@ class StageToRedshiftOperator(BaseOperator):
         FROM '{}'
         ACCESS_KEY_ID '{}'
         SECRET_ACCESS_KEY '{}'
-        IGNOREHEADER {}
-        DELIMITER '{}'
+        JSON '{}'
     """
+
 
     @apply_defaults
     def __init__(self,
@@ -22,29 +23,21 @@ class StageToRedshiftOperator(BaseOperator):
                 table="",
                 s3_bucket="",
                 s3_key="",
-                delimiter=",",
-                ignore_headers=1,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
+                json="",
+                 *args, **kwargs             
+                ):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
         self.table = table
         self.redshift_conn_id = redshift_conn_id
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
-        self.delimiter = delimiter
-        self.ignore_headers = ignore_headers
+        self.json=json
         self.aws_credentials_id = aws_credentials_id
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
-        metastoreBackend = MetastoreBackend()
-        aws_connection=metastoreBackend.get_connection(self.aws_credentials_id)
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
         self.log.info("Clearing data from destination Redshift table")
@@ -56,12 +49,22 @@ class StageToRedshiftOperator(BaseOperator):
         formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
-            aws_connection.login,
-            aws_connection.password,
-            self.ignore_headers,
-            self.delimiter
+            credentials.access_key,
+            credentials.secret_key,
+            self.json
         )
+
+        
+        self.log.info("creating staging table")
+        redshift.run(final_project_sql_statements.staging_events)
+        self.log.info("creating songs table")
+        redshift.run(final_project_sql_statements.staging_songs)
         redshift.run(formatted_sql)
+
+
+
+
+
 
 
 
